9ce2e09d90e5092397b2ebc1643b6d722531c977
renzozuk <renzo.zuk19@gmail.com>
2025-03-23 19:33:14 -0300

    Feat: RabbitMQ producer

---
+++ src/main/java/dev/renzozukeram/rabbitmq_study/publisher/RabbitMQProducer.java
@@ -0,0 +1,32 @@
+package dev.renzozukeram.rabbitmq_study.publisher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQProducer.class);
+
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        logger.info(String.format("Message sent: %s", message));
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
