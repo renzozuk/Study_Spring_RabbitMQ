a2d953c93e54431d5ef24ecd0558ea26135ca166
renzozuk <renzo.zuk19@gmail.com>
2025-03-23 19:22:00 -0300

    Feat: RabbitMQ config

---
+++ src/main/java/dev/renzozukeram/rabbitmq_study/RabbitmqStudyApplication.java
@@ -0,0 +1,13 @@
+package dev.renzozukeram.rabbitmq_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqStudyApplication.class, args);
+	}
+
+}
